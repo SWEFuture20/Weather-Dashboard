5e24536e6a0101cc4f89ff2b67ab07c51dff9b77
Latifah Lillie <queen1892@gmail.com>
2021-02-21 23:14:09 -0600

    Adding Ajax and Time from Moment.js

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function weatherGenerator(city){
+    var APIKEY = "1b3cfcbab3962e57d2f22e946221f380"
+    var queryURLOne = "http://api.openweathermap.org/data/2.5/forecast?q=Chicago&units=imperial&cnt=5&appid=" + APIKEY;
+    var queryURLTwo = ""
+    console.log(queryURLOne)
+    $.ajax({
+        url: queryURLOne,
+        method: "GET",
+      }).then(function(response) {
+        console.log(response.list[0].main)
+        console.log(response.list[0].main.temp)
+        console.log(response.list[0].main.humidity)
+
+        var time = moment().format("MMM Do YY");
+        console.log(time)
+});
+
+}
+weatherGenerator();
