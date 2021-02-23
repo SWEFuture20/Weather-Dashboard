e8722b3b997d1efa1682e98f43ac6481ad8f726b
Latifah Lillie <queen1892@gmail.com>
2021-02-22 22:29:16 -0600

    Finished First Weather Grrid

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-
+// var APIKEY = config.MY_KEY;
 function weatherGenerator(city){
-    var APIKEY = config.MY_KEY;
+    var APIKEY = '1b3cfcbab3962e57d2f22e946221f380';
     var queryURLOne = "http://api.openweathermap.org/data/2.5/forecast?q=Chicago&units=imperial&cnt=5&appid=" + APIKEY;
     var queryURLTwo = ""
     console.log(queryURLOne)
@@ -15,10 +15,42 @@ function weatherGenerator(city){
         var time = moment().format("MMM Do YY");
         var timeOne = $("<p>").text(time)
         var temp = $("<p>").text("Temp:" + response.list[0].main.temp)
-        var humidity =  $("<p>").text("Humidity:" + response.list[0].main.humidity)   
+        var humidity =  $("<p>").text("Humidity:" + response.list[0].main.humidity + '%')
+        var iconcode = response.list[0].weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        var IMGicon = $("<img>").attr("src", iconurl)
         console.log(time)
-        $(".temp-results").append(timeOne, temp);
+        $(".temp-results").append(timeOne, IMGicon, temp, humidity);
+
+        
 });
 
 }
 weatherGenerator();
+
+function timeOne(){
+    const currentMoment = moment().add(0, 'days');
+const endMoment = moment().add(6, 'days');
+while (currentMoment.isBefore(endMoment, 'day')) {
+  console.log(`${currentMoment.format('l')}`);
+  currentMoment.add(1, 'days');
+}
+}
+timeOne()
+
+function daysOfWeek(){
+    var start = new Date();
+var end = new Date();
+
+
+var loop = new Date(start);
+while(loop <= end){
+   console.log(loop);           
+
+   var newDate = loop.setDate(loop.getDate() + 1);
+   loop = new Date(newDate);
+} 
+
+}
+
+daysOfWeek();
